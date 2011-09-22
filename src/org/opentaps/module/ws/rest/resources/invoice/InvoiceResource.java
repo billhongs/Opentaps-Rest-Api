68cdaa33999f066cfa2c76788e5f57e947d67b19
alberto.lobrano <alberto.lobrano@integratingweb.com>
2011-09-22 11:52:12 +0200

    created test class for invoices (to test rest concepts),  created configuration directory and moved the configurations files and application files in to it

---
+++ src/org/opentaps/module/ws/rest/resources/invoice/InvoiceResource.java
@@ -0,0 +1,11 @@
+package org.opentaps.module.ws.rest.resources.invoice;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: alobrano
+ * Date: 22/09/2011
+ * Time: 11:13
+ * To change this template use File | Settings | File Templates.
+ */
+public class InvoiceResource {
+}

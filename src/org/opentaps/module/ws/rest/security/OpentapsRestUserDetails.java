1a323bb41cf34a0934a4ae69f693bd5520aff8c5
Alberto Lobrano <alberto.lobrano@integratingweb.com>
2011-09-29 18:19:32 +0200

    integration of spring security with ofbiz dao for login

---
+++ src/org/opentaps/module/ws/rest/security/OpentapsRestUserDetails.java
@@ -0,0 +1,113 @@
+package org.opentaps.module.ws.rest.security;
+
+import org.opentaps.foundation.infrastructure.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.GrantedAuthorityImpl;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: alobrano
+ * Date: 9/29/11
+ * Time: 5:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class OpentapsRestUserDetails  implements UserDetails {
+
+    private Collection<GrantedAuthority> authorities;
+    private String password;
+    private String username;
+    private boolean accountNonExpired;
+    private boolean accountNonLocked;
+    private boolean credentialsNonExpired;
+    private boolean enabled;
+    private User user;
+
+    public OpentapsRestUserDetails() {
+
+    }
+
+    public OpentapsRestUserDetails(Collection<GrantedAuthority> authorities, String password, String username, boolean accountNonExpired, boolean accountNonLocked, boolean credentialsNonExpired, boolean enabled) {
+        this.authorities = authorities;
+        this.password = password;
+        this.username = username;
+        this.accountNonExpired = accountNonExpired;
+        this.accountNonLocked = accountNonLocked;
+        this.credentialsNonExpired = credentialsNonExpired;
+        this.enabled = enabled;
+    }
+
+    public Collection<GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public boolean isAccountNonExpired() {
+        return accountNonExpired;
+    }
+
+    public boolean isAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    public boolean isCredentialsNonExpired() {
+        return credentialsNonExpired;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void addAuthority(String authorityString){
+        if(authorities==null){
+            authorities = new ArrayList<GrantedAuthority>(40);
+        }
+        authorities.add(new GrantedAuthorityImpl(authorityString));
+    }
+
+    public void setAuthorities(Collection<GrantedAuthority> authorities) {
+        this.authorities = authorities;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setAccountNonExpired(boolean accountNonExpired) {
+        this.accountNonExpired = accountNonExpired;
+    }
+
+    public void setAccountNonLocked(boolean accountNonLocked) {
+        this.accountNonLocked = accountNonLocked;
+    }
+
+    public void setCredentialsNonExpired(boolean credentialsNonExpired) {
+        this.credentialsNonExpired = credentialsNonExpired;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
---
+++ src/org/opentaps/module/ws/rest/security/OpentapsUserDetailService.java
@@ -0,0 +1,80 @@
+package org.opentaps.module.ws.rest.security;
+
+import org.ofbiz.base.util.Debug;
+import org.ofbiz.entity.Delegator;
+import org.ofbiz.entity.DelegatorFactory;
+import org.ofbiz.service.GenericDispatcher;
+
+import org.opentaps.base.entities.UserLogin;
+import org.opentaps.base.entities.UserLoginSecurityGroup;
+import org.opentaps.foundation.entity.EntityNotFoundException;
+import org.opentaps.foundation.infrastructure.Infrastructure;
+import org.opentaps.foundation.infrastructure.User;
+import org.opentaps.foundation.repository.RepositoryException;
+import org.opentaps.foundation.repository.ofbiz.Repository;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: alobrano
+ * Date: 9/29/11
+ * Time: 4:19 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class OpentapsUserDetailService implements UserDetailsService {
+
+    private static final String MODULE = OpentapsUserDetailService.class.getName() ;
+    private final static String DELEGATOR_NAME = "default";
+    private static String DISPATCHER_NAME = "default";
+
+
+
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException, DataAccessException {
+        Repository repository = new Repository();
+        Delegator delegator = DelegatorFactory.getDelegator(DELEGATOR_NAME);
+        repository.setInfrastructure(new Infrastructure(GenericDispatcher.getLocalDispatcher(DISPATCHER_NAME, delegator)));
+        UserLogin userLogin = null;
+        try {
+            userLogin = repository.findOneNotNull(UserLogin.class, repository.map(UserLogin.Fields.userLoginId, username));
+        } catch (RepositoryException e) {
+           //todo do something with this error.
+            Debug.logError("Error communicating with the Repository",MODULE);
+        } catch (EntityNotFoundException e) {
+           throw new UsernameNotFoundException("User ["+username+"] not found",e);
+        }
+
+
+        OpentapsRestUserDetails opentapsRestUserDetails = new OpentapsRestUserDetails();
+        opentapsRestUserDetails.setUsername(userLogin.getUserLoginId());
+        opentapsRestUserDetails.setPassword(userLogin.getCurrentPassword());
+        opentapsRestUserDetails.setAccountNonExpired(userLogin.getEnabled() == null ? true : userLogin.getEnabled().equals("Y"));
+        opentapsRestUserDetails.setAccountNonLocked(userLogin.getEnabled()== null ? true : userLogin.getEnabled().equals("Y"));
+        opentapsRestUserDetails.setCredentialsNonExpired(userLogin.getEnabled()== null ? true : userLogin.getEnabled().equals("Y"));
+        opentapsRestUserDetails.setEnabled(userLogin.getEnabled()== null ? true : userLogin.getEnabled().equals("Y"));
+        List<? extends UserLoginSecurityGroup> userLoginSecurityGroups = null;
+        try {
+            userLoginSecurityGroups = userLogin.getUserLoginSecurityGroups();
+        } catch (RepositoryException e) {
+            //todo do something with this error.
+            Debug.logError("Error communicating with the Repository",MODULE);
+        }
+        for (UserLoginSecurityGroup userLoginSecurityGroup : userLoginSecurityGroups) {
+            opentapsRestUserDetails.addAuthority(userLoginSecurityGroup.getGroupId());
+        }
+
+        try {
+            opentapsRestUserDetails.setUser(new User(Repository.genericValueFromEntity(userLogin),delegator));
+        } catch (RepositoryException e) {
+           //todo do something with this error.
+            Debug.logError("Error communicating with the Repository",MODULE);
+        }
+
+        return opentapsRestUserDetails;
+    }
+}

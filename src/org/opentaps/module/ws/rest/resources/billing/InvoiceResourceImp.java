a7520e33548c592dd842f4a7eb695e5385a5cb85
Alberto Lobrano <alberto.lobrano@integratingweb.com>
2011-10-07 20:27:35 +0200

    implementation of the billing domain:  invoices retrieve, invoice retrieve by invoice id , started invoice creation

--- src/org/opentaps/module/ws/rest/resources/billing/InvoiceResourceImp.java
+++ src/org/opentaps/module/ws/rest/resources/billing/InvoiceResourceImp.java
@@ -1,15 +1,14 @@
 package org.opentaps.module.ws.rest.resources.billing;
 
-import org.apache.wink.common.annotations.Workspace;
-import org.opentaps.base.entities.InvoiceItem;
+import org.opentaps.base.services.CreateInvoiceService;
 import org.opentaps.domain.billing.BillingDomainInterface;
 import org.opentaps.domain.billing.invoice.Invoice;
+import org.opentaps.foundation.entity.EntityNotFoundException;
 import org.opentaps.foundation.repository.RepositoryException;
 import org.opentaps.module.ws.rest.Context.OpentapsContext;
 import org.opentaps.module.ws.rest.asset.billing.InvoiceAsset;
 import org.opentaps.module.ws.rest.asset.billing.InvoicesAsset;
 import org.opentaps.module.ws.rest.domain.billing.InvoiceBean;
-import org.opentaps.module.ws.rest.domain.billing.InvoiceItemBean;
 import org.opentaps.module.ws.rest.domain.billing.InvoicesBean;
 import org.opentaps.module.ws.rest.errors.NoPermissionException;
 import org.opentaps.module.ws.rest.errors.RestApiException;
@@ -31,7 +30,6 @@ public class InvoiceResourceImp extends ApiAbstractResource implements InvoiceRe
     protected static String MODULE = InvoiceResourceImp.class.getName();
 
 
-
     public InvoicesAsset getInvoices(@Context OpentapsContext opentapsContext) throws RepositoryException, RestApiException {
 
         //make sure we have the right permissions
@@ -41,7 +39,7 @@ public class InvoiceResourceImp extends ApiAbstractResource implements InvoiceRe
         List<Invoice> invoices = billingDomain.getInvoiceRepository().findAll(Invoice.class);
         InvoicesBean invoicesBean = new InvoicesBean();
         for (Invoice invoice : invoices) {
-            InvoiceBean invoiceBean = getMapper().map(invoice,InvoiceBean.class);
+            InvoiceBean invoiceBean = getMapper().map(invoice, InvoiceBean.class);
             invoicesBean.getInvoices().add(invoiceBean);
         }
         return new InvoicesAsset(invoicesBean);
@@ -52,38 +50,51 @@ public class InvoiceResourceImp extends ApiAbstractResource implements InvoiceRe
                                    @PathParam("id") String invoiceId) throws NoPermissionException, RepositoryException {
         //make sure we have the right permissions
         super.checkPermission(opentapsContext.getSecurityManager(), "PERMISSION", "invoices");
-//
-//        BillingDomainInterface billingDomain = opentapsContext.getDomainDirectory().getBillingDomain();
-//        Invoice invoiceById = null;
-//        try {
-//            invoiceById = billingDomain.getInvoiceRepository().getInvoiceById(invoiceId);
-//        } catch (EntityNotFoundException e) {
-//            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-//        }
-//
-//        return new InvoiceAsset(getMapper().map(invoiceById,InvoiceXml.class));
-        return null;
+
+        //todo IW check that the invoice id is populated
+
+        return getInvoiceById(opentapsContext,invoiceId);
     }
 
 
     public InvoiceAsset createInvoice(InvoiceAsset invoiceAsset, @Context OpentapsContext opentapsContext) throws NoPermissionException, Exception {
         //make sure we have the right permissions
-//        super.checkPermission(opentapsContext.getSecurityManager(), "PERMISSION", "invoices");
-//
-//        CreateInvoiceService createInvoiceService = new CreateInvoiceService();
-//        InvoiceXml invoiceXml = invoiceAsset.getInvoice();
-//        createInvoiceService.setInInvoiceTypeId(invoiceXml.getInvoiceTypeId());
-//
-//        createInvoiceService.setUser(opentapsContext.getSecurityManager().getOpentapsUser());
-//
-//        createInvoiceService.runSync(opentapsContext.getInfrastructure());
-//
-//        String outInvoiceId = createInvoiceService.getOutInvoiceId();
-//
-//        Invoice invoice = opentapsContext.getDomainDirectory().getBillingDomain().getInvoiceRepository().getInvoiceById(outInvoiceId);
-//
-//
-//        return new InvoiceAsset(getMapper().map(invoice,InvoiceXml.class));
-        return null;
+        super.checkPermission(opentapsContext.getSecurityManager(), "PERMISSION", "invoices");
+
+        //todo IW we may do this in the mapper conf
+        CreateInvoiceService createInvoiceService = new CreateInvoiceService();
+        InvoiceBean invoiceBean = invoiceAsset.getInvoice();
+
+        createInvoiceService.setInInvoiceTypeId(invoiceBean.getInvoiceTypeId());
+        createInvoiceService.setInPartyIdFrom(invoiceBean.getPartyIdFrom());
+        createInvoiceService.setInPartyId(invoiceBean.getPartyId());
+        createInvoiceService.setInCurrencyUomId(invoiceBean.getCurrencyUomId());
+
+        createInvoiceService.setUser(opentapsContext.getSecurityManager().getOpentapsUser());
+        createInvoiceService.runSync(opentapsContext.getInfrastructure());
+
+        String outInvoiceId = createInvoiceService.getOutInvoiceId();
+
+        return getInvoiceById(opentapsContext,outInvoiceId);
+
+    }
+
+
+    private InvoiceAsset getInvoiceById(OpentapsContext opentapsContext, String invoiceId) {
+
+        BillingDomainInterface billingDomain = opentapsContext.getDomainDirectory().getBillingDomain();
+        Invoice invoiceById = null;
+        try {
+            invoiceById = billingDomain.getInvoiceRepository().getInvoiceById(invoiceId);
+        } catch (EntityNotFoundException e) {
+            //todo IW do something with this error
+            e.printStackTrace();
+
+        } catch (RepositoryException e) {
+            //todo IW do something with this error
+            e.printStackTrace();
+        }
+        return new InvoiceAsset(getMapper().map(invoiceById, InvoiceBean.class));
+
     }
 }
